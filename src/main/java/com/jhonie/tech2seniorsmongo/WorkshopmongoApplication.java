549cda667dce7a4e64768326c624722aa682eacb
Alan Jhonie <alanjhonie2@gmail.com>
2023-09-14 23:49:09 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/jhonie/tech2seniorsmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.jhonie.tech2seniorsmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhonie/tech2seniorsmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonie.tech2seniorsmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
